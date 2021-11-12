66b228d6a184feae1453947d55fdba3be6a92ed7
a.shilin <a.shilin@adeptum.ru>
2021-11-12 14:40:23 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import endOfYear from 'date-fns/endOfYear';
+import getYear from 'date-fns/getYear';
 
-function App() {
+const histogramRows = [
+    {
+        id: 1,
+        dateFrom: 'Wed, 01 Oct 2019 07:59:00 GMT',
+        dateTo: 'Wed, 01 Dec 2020 07:59:00 GMT',
+        background: 'blue',
+    },
+    {
+        id: 2,
+        dateFrom: 'Wed, 01 May 2020 07:59:00 GMT',
+        dateTo: 'Wed, 03 Jun 2021 07:59:00 GMT',
+        background: 'green',
+    },
+    {
+        id: 3,
+        dateFrom: 'Wed, 01 Apr 2020 07:59:00 GMT',
+        dateTo: 'Wed, 03 Dec 2022 07:59:00 GMT',
+        background: 'red',
+    },
+    {
+        id: 4,
+        dateFrom: 'Wed, 21 Apr 2021 07:59:00 GMT',
+        dateTo: 'Wed, 03 Dec 2021 07:59:00 GMT',
+        background: 'brown',
+    },
+    {
+        id: 5,
+        dateFrom: 'Wed, 01 Feb 2021 07:59:00 GMT',
+        dateTo: 'Wed, 03 Dec 2024 07:59:00 GMT',
+        background: 'yellow',
+    },
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    </>
   );
 }
 
